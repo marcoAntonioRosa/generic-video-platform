125678afbc2587ec85829916cb4b6c89366e87a6
Marco Antonio <profissional.marco.silva@gmail.com>
2026-03-06 18:44:12 -0300

    - Added ng-bootstrap and ng- bootrstrap-icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapHandThumbsUp, bootstrapHandThumbsDown } from '@ng-icons/bootstrap-icons';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIconComponent],
+  providers: [provideIcons({ bootstrapHandThumbsUp, bootstrapHandThumbsDown })],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
