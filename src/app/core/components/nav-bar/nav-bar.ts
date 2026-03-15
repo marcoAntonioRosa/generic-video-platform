b7eb82dc05a0d273fe9b7a5ffc940442702d57d2
Marco Antonio <profissional.marco.silva@gmail.com>
2026-03-15 11:21:34 -0300

    - Simplified the navbar by removing rows and columns and adding flexbox; - Added a collapse button so the search bar and the user action icons are hidden on smaller screens - Improved the layout for smaller devices

--- src/app/core/components/nav-bar/nav-bar.ts
+++ src/app/core/components/nav-bar/nav-bar.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap/collapse';
+import { RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 import { bootstrapList, bootstrapRocketTakeoff, bootstrapSearch, bootstrapMic, bootstrapPlusLg, bootstrapBell, bootstrapPerson } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgIconComponent],
+  imports: [NgbCollapse, RouterLink, NgIconComponent],
   providers: [provideIcons({ bootstrapList, bootstrapRocketTakeoff, bootstrapSearch, bootstrapMic, bootstrapPlusLg, bootstrapBell, bootstrapPerson })],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar { }
+export class NavBar {
+  isCollapsed = false;
+  isMenuCollapsed = true;
+}
