c4696edb8c2189838e297e4928666c5504e55ca9
Marco Antonio <profissional.marco.silva@gmail.com>
2026-03-28 15:28:49 -0300

    - Added more youtube-like details to the video details page

--- src/app/features/video/components/video-details/video-details.component.ts
+++ src/app/features/video/components/video-details/video-details.component.ts
@@ -1,8 +1,18 @@
 import { Component, Input } from '@angular/core';
+import {
+  NgbDropdown,
+  NgbDropdownToggle,
+  NgbDropdownMenu,
+  NgbDropdownItem,
+  NgbDropdownButtonItem
+} from '@ng-bootstrap/ng-bootstrap/dropdown';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapHandThumbsUp, bootstrapHandThumbsDown, bootstrapShare, bootstrapBookmarkPlus } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-video-details',
-  imports: [],
+  imports: [NgIconComponent, NgbDropdown, NgbDropdownToggle, NgbDropdownMenu, NgbDropdownItem, NgbDropdownButtonItem],
+  providers: [provideIcons({ bootstrapHandThumbsUp, bootstrapHandThumbsDown, bootstrapShare, bootstrapBookmarkPlus })],
   templateUrl: './video-details.component.html',
   styleUrl: './video-details.component.css',
 })
